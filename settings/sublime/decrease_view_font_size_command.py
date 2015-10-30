072605bb02fc5c4570a0ee3bebd67563a072dd09
Maic Lopez Saenz <maic@lopsae.com>
2015-10-30 15:54:40 -0700

    add command to reset font size of a sublime view

--- settings/sublime/decrease_view_font_size_command.py
+++ settings/sublime/decrease_view_font_size_command.py
@@ -5,6 +5,6 @@ import sublime_plugin
 class DecreaseViewFontSizeCommand(sublime_plugin.ApplicationCommand):
 	def run(self):
 		view_settings = sublime.active_window().active_view().settings()
-		current_size = view_settings.get("font_size")
+		current_size = view_settings.get('font_size')
 		if current_size > 1:
-			view_settings.set("font_size", current_size-1)
+			view_settings.set('font_size', current_size-1)
